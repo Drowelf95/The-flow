cda84bd19cfbda4733d8424fb60988eff4d5a8c9
Drowelf95 <sutter.johan@gmail.com>
2020-10-06 11:31:48 +0200

    Start

--- src/Controller/BackController.php
+++ src/Controller/BackController.php
@@ -8,11 +8,21 @@ use Symfony\Component\Routing\Annotation\Route;
 class BackController extends AbstractController
 {
     /**
-     * @Route("/back", name="back")
+     * @Route("/login", name="Login")
      */
-    public function index()
+    public function Login()
     {
-        return $this->render('back/index.html.twig', [
+        return $this->render('back/Login.html.twig', [
+            'controller_name' => 'BackController',
+        ]);
+    }
+
+    /**
+     * @Route("/test", name="test")
+     */
+    public function Test()
+    {
+        return $this->render('back/test.html.twig', [
             'controller_name' => 'BackController',
         ]);
     }
