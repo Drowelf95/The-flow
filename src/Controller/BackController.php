8f0810bead088d77d9d44cafc0d4bd34910a2eac
Drowelf95 <sutter.johan@gmail.com>
2020-10-09 17:42:31 +0200

    Updates

--- src/Controller/BackController.php
+++ src/Controller/BackController.php
@@ -18,12 +18,52 @@ class BackController extends AbstractController
     }
 
     /**
-     * @Route("/test", name="test")
+     * @Route("/splashPage", name="splashPage")
      */
-    public function Test()
+    public function splashPage()
     {
-        return $this->render('back/test.html.twig', [
+        return $this->render('back/splashPage.html.twig', [
             'controller_name' => 'BackController',
         ]);
+    }
+
+    /**
+     * @Route("/segmentView", name="segmentView")
+     */
+    public function segmentView()
+    {
+        return $this->render('back/segmentView.html.twig', [
+            'controller_name' => 'BackController',
+        ]);
+    }
+        
+    /**
+     * @Route("/segmentAdd", name="segmentAdd")
+     */
+    public function segmentAdd()
+    {
+        return $this->render('back/segmentAdd.html.twig', [
+            'controller_name' => 'BackController',
+        ]);
+    }
+
+    /**
+     * @Route("/segmentEdit", name="segmentEdit")
+     */
+    public function segmentEdit()
+    {
+        return $this->render('back/segmentEdit.html.twig', [
+            'controller_name' => 'BackController',
+        ]);
+    }
+    
+    /**
+     * @Route("/logOut", name="logOut")
+     */
+    public function logOut()
+    {
+        /*return $this->render('back/logOut.html.twig', [
+            'controller_name' => 'BackController',
+        ]);*/
     }
 }
